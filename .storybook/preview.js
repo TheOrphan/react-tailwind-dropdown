c103d740395a9c992971dc358d92f0ea1c681c8e
joko-santoso <hello.jokosantoso@gmail.com>
2025-02-05 01:52:07 +0700

    Updates

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import "../src/styles/tailwind.css";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className="p-4">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default preview;
